e9d5b9eb3a50d108f08cbf973d8ce92d45bceedc
Vishnu Narayanan <vishnu777aero@gmail.com>
2024-11-13 15:31:50 -0500

    remove C# files

--- Battleship/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Battleship.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
