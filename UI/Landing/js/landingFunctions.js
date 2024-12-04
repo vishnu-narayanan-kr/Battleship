e3750b5d779b7ede9c2bc018ab4ba0d92a6aec0b
Vishnu Narayanan <vishnu777aero@gmail.com>
2024-12-04 12:32:04 -0500

    Integrate Landing Page to Placement Page

--- UI/Landing/js/landingFunctions.js
+++ UI/Landing/js/landingFunctions.js
@@ -6,3 +6,12 @@ function showSettings() {
 function closePopUpsMenus() {
     document.getElementById("settingsPage").style.display = "none";
 }
+
+const onPlayOnline = () => {
+    // authentication will be done at a later phase
+
+    const username = document.getElementById("username").value;
+    console.log({ username });
+
+    window.location.replace("http://" + location.hostname + ":5500/UI/Placement/index.html?username=" + username);
+}
